0e53263e846e8caf5bca8eb2e79ab2b46e3162d9
Matt Crisp <mcrisp1972@att.net>
2026-01-30 23:53:22 -0800

    moving theme panel to parent theme

--- src/scripts/block-editor-hooks.js
+++ src/scripts/block-editor-hooks.js
@@ -1,6 +1,12 @@
 import { addFilter } from '@wordpress/hooks';
+import { registerPlugin } from '@wordpress/plugins';
+import { default as themePanels } from './sidebar-panels/theme-panel';
 import defaultColorThemes from '../../color-themes.json';
 import './core-blocks';
+
+registerPlugin( 'cwps-sidebar-theme', {
+	render: themePanels,
+} );
 
 addFilter( 'cwps.colorThemes', 'cwps/color-themes', () => {
     return defaultColorThemes;
