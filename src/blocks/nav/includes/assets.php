7a741dec975e9a291810852bd6defbda1b80a6c5
Matt Crisp <mcrisp1972@att.net>
2026-02-01 11:01:12 -0800

    nav editor style id update

--- src/blocks/nav/includes/assets.php
+++ src/blocks/nav/includes/assets.php
@@ -7,5 +7,5 @@ add_action( 'enqueue_block_assets', __NAMESPACE__ . '\register_assets', 10 );
 function register_assets() {
 
 	wp_register_style( 'capitola-nav', CAPITOLA_BLOCKS_URL . 'nav/style-index.css', array( CAPITOLA_STYLE_DEP ), CAPITOLA_THEME_VER );
-	wp_register_style( 'capitola-navAdmin', CAPITOLA_BLOCKS_URL . 'nav/index.css', array( CAPITOLA_STYLE_DEP ), CAPITOLA_THEME_VER );
+	wp_register_style( 'capitola-nav-editor', CAPITOLA_BLOCKS_URL . 'nav/index.css', array( CAPITOLA_STYLE_DEP ), CAPITOLA_THEME_VER );
 }
