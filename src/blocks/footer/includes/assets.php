1a71aa8d865774307ea057d6eabba123fd451b00
Matt Crisp <mcrisp1972@att.net>
2026-02-01 11:46:33 -0800

    footer style id renames

--- src/blocks/footer/includes/assets.php
+++ src/blocks/footer/includes/assets.php
@@ -6,5 +6,5 @@ add_action( 'enqueue_block_assets', __NAMESPACE__ . '\register_assets' );
 
 function register_assets() {
 	wp_register_style( 'capitola-footer', CAPITOLA_BLOCKS_URL . 'footer/style-index.css', array( CAPITOLA_STYLE_DEP ), CAPITOLA_THEME_VER );
-	wp_register_style( 'capitola-footerAdmin', CAPITOLA_BLOCKS_URL . 'footer/index.css', array( CAPITOLA_STYLE_DEP, 'capitola-footer' ), CAPITOLA_THEME_VER );
+	wp_register_style( 'capitola-footer-editor', CAPITOLA_BLOCKS_URL . 'footer/index.css', array( CAPITOLA_STYLE_DEP, 'capitola-footer' ), CAPITOLA_THEME_VER );
 }
