978322239bc67d1113f51bc1f228f985ad929430
Matt Crisp <mcrisp1972@att.net>
2026-02-05 12:40:13 -0800

    test

--- includes/posts/posts-admin-columns.php
+++ includes/posts/posts-admin-columns.php
@@ -5,5 +5,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 add_filter( 'manage_edit-category_columns', '\Capitola\Admin_Terms_Listings\tax_thumb_col_head' );
-
 add_filter( 'manage_category_custom_column', '\Capitola\Admin_Terms_Listings\tax_thumb_col', 10, 3 );
