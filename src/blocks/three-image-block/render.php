ef993a8a6fdf82512d259dd9ed206b5b97b032b6
Matt Crisp <mcrisp1972@att.net>
2026-02-12 10:41:20 -0800

    Squashed 'capitola/' changes from d0c66e5..cc7b28b
    cc7b28b three image block
    
    git-subtree-dir: capitola
    git-subtree-split: cc7b28bb1cc047fb34c321d48a0250fb95dbdadf

---
+++ src/blocks/three-image-block/render.php
@@ -0,0 +1,35 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use function Capitola\Helpers\Block_Attributes\animation_attributes;
+
+$animations = animation_attributes( $attributes );
+
+$wrapper_attributes = get_block_wrapper_attributes(
+	array(
+		'id'    => $attributes['anchor'],
+		'class' => 'alignfull is-layout-constrained has-global-padding --theme-' . $attributes['colorTheme'],
+	)
+);
+
+?>
+
+<section <?php echo wp_kses_data( $wrapper_attributes ); ?>>
+	<div class="wp-block-capitola-three-image-block__width alignwide --has-<?php echo esc_attr( $attributes['introAlign'] ); ?>-intro <?php echo esc_attr( $animations['block-class'] ); ?><?php echo ( 'top' === $attributes['verticalAlign'] ? ' --align-top' : '' ); ?>" <?php echo wp_kses_data( $animations['block-data'] ); ?>>
+		<?php echo wp_kses_post( $content ); ?>
+		<div class="wp-block-capitola-three-image-block__imagecol --aspect-ratio-<?php echo esc_attr( $attributes['gridAspectRatio'] ); ?> <?php echo esc_attr( $animations['figure-class'] ); ?> --rear-position-<?php echo esc_attr( $attributes['rearImagePosition'] ); ?>" <?php echo wp_kses_data( $animations['figure-data'] ); ?>>
+			<figure class="wp-block-capitola-three-image-block__rear-image <?php echo $attributes['rearImageRadius'] ? ' --has-' . esc_attr( $attributes['rearImageRadius'] ) . '-radius' : ''; ?>" style=" --image-height: <?php echo esc_attr( $attributes['rearImageHeight'] ); ?>; --image-width:<?php echo esc_attr( $attributes['rearImageWidth'] ); ?>; --capitola-objectPosition:<?php echo esc_attr( $attributes['rearImageCropPosition'] ); ?>;">
+				<?php echo wp_get_attachment_image( $attributes['rearImage']['id'], 'large' ); ?>
+			</figure>
+			<figure class="wp-block-capitola-three-image-block__middle-image <?php echo $attributes['middleImageRadius'] ? ' --has-' . esc_attr( $attributes['middleImageRadius'] ) . '-radius' : ''; ?>" style=" --image-top-pos: <?php echo esc_attr( $attributes['middleImageTopPos'] ); ?>; --image-left-pos: <?php echo esc_attr( $attributes['middleImageLeftPos'] ); ?>; --image-height: <?php echo esc_attr( $attributes['middleImageHeight'] ); ?>; --image-width:<?php echo esc_attr( $attributes['middleImageWidth'] ); ?>; --capitola-objectPosition:<?php echo esc_attr( $attributes['middleImageCropPosition'] ); ?>;">
+				<?php echo wp_get_attachment_image( $attributes['middleImage']['id'], 'large' ); ?>
+			</figure>
+			<figure class="wp-block-capitola-three-image-block__front-image <?php echo $attributes['frontImageRadius'] ? ' --has-' . esc_attr( $attributes['frontImageRadius'] . '-radius' ) : ''; ?>" style=" --image-height: <?php echo esc_attr( $attributes['frontImageHeight'] ); ?>; --image-width: <?php echo esc_attr( $attributes['frontImageWidth'] ); ?>; --capitola-objectPosition:<?php echo esc_attr( $attributes['frontImageCropPosition'] ); ?>">
+				<?php echo wp_get_attachment_image( $attributes['frontImage']['id'], 'large' ); ?>
+			</figure>
+		</div>
+	</div>
+</section>
