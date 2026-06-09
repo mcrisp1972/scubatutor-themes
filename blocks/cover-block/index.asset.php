d4c7d2e0404e87e266a3f7a38e07966612fbfc22
mcrisp1972 <73559892+mcrisp1972@users.noreply.github.com>
2026-06-09 22:08:34 +0000

    Build: (9e69bed) Merge commit 'a16cdcde4fc9de2a31a42b48ef87209b0c022292'

--- blocks/cover-block/index.asset.php
+++ blocks/cover-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-keycodes', 'wp-primitives'), 'version' => 'c26b4ace965fedffb5db');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-keycodes', 'wp-primitives'), 'version' => '0cf51f90b43c3db6b3f5');
--- styles/blocks/listings-filters.asset.php
+++ styles/blocks/listings-filters.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'b30fba7265c4019420c7');
+<?php return array('dependencies' => array(), 'version' => 'eb70a676b960d342a91c');
